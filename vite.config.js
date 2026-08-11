dee3691d68b0243710a468d528ea07a7244e0056
GUILHERME BARRIM TEIXEIRA <guilherme.teixeira@alunos.sc.senac.br>
2026-08-10 21:20:58 -0300

    secound commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './GuilhermeTeixeira-landing-page/',
+  base: '/GuilhermeTeixeira-landing-page/',
   build: {
     outDir: 'docs', 
   }
